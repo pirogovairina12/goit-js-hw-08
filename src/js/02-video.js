0d88ac6144a7bf70199abb6689be0abb721ebbd3
Anastasia Pirogova <120117437+pirogovairina12@users.noreply.github.com>
2023-05-05 21:53:17 +0300

    summary

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,61 +1,19 @@
 import Player from '@vimeo/player';
-//import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const KEY_PLAYER = "videoplayer-current-time";
 
-console.log(localStorage);
-
-// localStorage.setItem(KEY_PLAYER);
-
-const savedTime = localStorage.getItem(KEY_PLAYER);
-console.log('savedTime', savedTime);
-
-const parsedData = JSON.parse(savedTime);
-console.log('parsedTime', parsedData);
+player.setCurrentTime(localStorage.getItem(KEY_PLAYER) || 0);
 
 
-// player.setCurrentTime(localStorage.getItem(KEY_PLAUER) || 0);
-//
-// player.on('play', function() {
-//   console.log('played the video!');
-// });
-//
-// player.getVideoTitle().then(function(title) {
-//   console.log('title:', title);
-// });
-//
-// const onPlay = function(data) {
-//
-// };
-//
-// player.on('play', onPlay);
-
-
-/*
-* Задание 2 - видео плеер
-В HTML есть <iframe> с видео для Vimeo плеера. Напиши скрипт который будет сохранять текущее время воспроизведения видео в локальное хранилище и, при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
-
-<iframe
-  id="vimeo-player"
-  src="https://player.vimeo.com/video/236203659"
-  width="640"
-  height="360"
-  frameborder="0"
-  allowfullscreen
-  allow="autoplay; encrypted-media"
-></iframe>
-
-Выполняй это задание в файлах 02-video.html и 02-video.js. Разбей его на несколько подзадач:
-
-Ознакомься с документацией библиотеки Vimeo плеера.
-Добавь библиотеку как зависимость проекта через npm.
-Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
-Разбери документацию метода on() и начни отслеживать событие timeupdate - обновление времени воспроизведения.
-Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища будет строка "videoplayer-current-time".
-При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы возобновить воспроизведение с сохраненной позиции.
-Добавь в проект библиотеку lodash.throttle и сделай так, чтобы время воспроизведения обновлялось в хранилище не чаще чем раз в секунду.
-* */
+const playerTime = function () {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(KEY_PLAYER, seconds);
+    console.log('playerTime');
+  });
+};
+player.on('timeupdate', throttle(playerTime, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,56 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+  button:  document.querySelector('button'),
+};
+
+refs.input.addEventListener('input', throttle(onTextareaInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateTextarea();
+populateForm();
+
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function onTextareaInput(evt) {
+  const email = refs.input.value;
+   const message = refs.textarea.value;
+   const data = {email, message};
+
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  console.log(data)
+}
+
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const savedEmail = localStorage.getItem(STORAGE_KEY);
+
+  if (savedMessage) {
+    refs.textarea.value = savedMessage;
+    refs.input.value =  savedEmail;
+  }
+}
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    refs.input.value = formData.email;
+    refs.textarea.value = formData.message;
+  }
+}
+
 
